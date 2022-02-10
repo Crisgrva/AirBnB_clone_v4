129e4aa8f08f744c287efaf2ac211297b19f3164
YaelUribe <yael3x@gmail.com>
2022-02-09 23:24:29 -0500

    corrected some things. Implemented the checkbox checker, if it its checked it stores the value in a list, then sends it to convert it into an array

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,16 @@
 let amenities = [];
-function ToArray (amenities) { // storing first all amenities
+function ToArray (amenities) { // wrong this is to take the list im creating below
   $('.amenities H4').text(amenities.join(', ')); // taking h4 text contents into list
-}
+}  // this function turnsthe list to an array
 
 function checkbox () {
-  $('.amenities input').on(); // this is useful to check on checkboxes
+  $('.amenities input').on('change', function () {
+    let checker = $(this)[0].checked; // take the element to see if it is checked
+    if (checker == 'true') {
+      amenities.push($(this).data('name')) // storing value
+    }
+    ToArray(amenities) // sending to convert list to array
+  });
 }
 
 $(document).ready(checkbox);
