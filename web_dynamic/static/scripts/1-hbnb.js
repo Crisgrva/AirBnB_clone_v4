f95db2426b4158886eaace17d76a42ee97e4c148
YaelUribe <yael3x@gmail.com>
2022-02-09 23:09:32 -0500

    this looks better but still missing a lot. just thought of taking first the whole list of elements and store them in a list so I can later parse it to check if the checkbox is selected or not

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,10 @@
-#!/usr/bin/node
+let amenities = [];
+function ToArray (amenities) { // storing first all amenities
+  $('.amenities H4').text(amenities.join(', ')); // taking h4 text contents into list
+}
 
-function checkbox()
-{
-    return [$("input").toString()]
-};
+function checkbox () {
+  $('.amenities input').on(); // this is useful to check on checkboxes
+}
+
+$(document).ready(checkbox);
